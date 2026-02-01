72e940793b06a6f52e43f38df4c0297a8a347495
charles wan <chawlesw@gmail.com>
2026-01-31 21:34:36 -0500

    Extra Credit Code

---
+++ extra_credit/loop_practice_a_wan.cpp
@@ -0,0 +1,112 @@
+/*
+Extra Credit: Loop Practice
+Part A: Practice
+
+By: Charles Wan
+EECE2140, Professor Nafa
+31 January, 2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    //PROBLEM 1
+    // int i = 1;
+    // while(i <= 10)
+    // {
+    //     cout << i << " ";
+    //     i++;
+    // }
+    // cout << endl;
+
+    //PROBLEM 2
+    // int i = 10;
+    // while(i >= 1)
+    // {
+    //     cout << i << " ";
+    //     i--;
+    // }
+    // cout << endl;
+
+    //PROBLEM 3
+    // int i = 0;
+    // do
+    // {
+    //     i += 2;
+    //     cout << i << " ";
+    // } 
+    // while(i < 20);
+    // cout << endl;
+
+    //PROBLEM 4
+    // int i = 5;
+    // do
+    // {
+    //     cout << i << " ";
+    //     i -= 1;
+    // } 
+    // while(i >= 1);
+    // cout << endl;
+
+    //PROBLEM 5
+    // int sum = 0;
+    // int user_input = 1;
+    // while(user_input != 0){
+    //     cout << "Type a number: ";
+    //     cin >> user_input;
+    //     sum += user_input;
+    // }
+    // cout << "Sum: " << sum << endl;
+
+    //PROBLEM 6
+    // int i = 0;
+    // while(i < 60){
+    //     i += 3;
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    //PROBLEM 7
+    // int i = 0;
+    // while(i <= 4){
+    //     cout << i << " ";
+    //     i += 1;
+    // }
+    // cout << endl;    
+
+    //PROBLEM 8
+    // int i = 19;
+    // do
+    // {
+    //     cout << i << " ";
+    //     i -= 2;
+    // } 
+    // while(i >= 1);
+    // cout << endl; 
+
+    //PROBLEM 9
+    // int pos_num = 0;
+    // int x;
+    // cout << "Type a positive number: ";
+    // cin >> pos_num;
+    // while (pos_num != 0)
+    // {
+    //     x = pos_num % 10;
+    //     cout << x;
+    //     pos_num = pos_num/10;
+    // }
+    // cout << endl;
+
+    //PROBLEM 10
+    // int n = 1;
+    // while(n <= 100)
+    // {
+    //     cout << n << " ";
+    //     n = n * 2;
+    // }
+    // cout << endl;
+
+    return 0;
+}
---
+++ extra_credit/loop_practice_b_wan.cpp
@@ -0,0 +1,94 @@
+/*
+Extra Credit: Loop Practice
+Part B: Fix-the-bug
+
+By: Charles Wan
+EECE2140, Professor Nafa
+31 January, 2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    //BUG 1
+    // int i = 1;
+    // while (i<=10)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    //BUG 2
+    // int i = 10;
+    // while (i > 1)
+    // {
+    //     cout << i << " ";
+    //     i--;
+    // }
+    // cout << endl;
+
+    //BUG 3
+    // int i = 1;
+    // while (i <= 5)
+    // {
+    //     cout << i << " ";
+    //     i++;
+    // }
+    // cout << endl;
+
+    //BUG 4
+    // int i = 0;
+    // do{
+    //     cout << i << " ";
+    //     i++;
+    // } while( i <= 4);
+
+    //BUG 5
+    // int i = 10;
+    // while(i >= 5){
+    //     cout << i << endl;
+    //     i--;
+    // }
+
+    //BUG 6
+    // int i = 0;
+    // while(i <= 10){
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    //BUG 7
+    // int i = 1;
+    // do{
+    //     cout << i << " ";
+    //     i++;
+    // } while(i <= 10);
+
+    //BUG 8
+    // int x = 1;
+    // int sum = 0;
+    // while(x != 0){
+    //     cin >> x;
+    //     if(x != 0){
+    //         sum += x;
+    //     }
+    // }
+    // cout << "Sum: " << sum << endl;
+
+    //BUG 9
+    // int i = 5;
+    // while(i >= 0){
+    //     cout << i << " ";
+    //     i--;
+    // }
+
+    //BUG 10
+    // int i = 0;
+    // while(i < 5){
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    return 0;
+}
